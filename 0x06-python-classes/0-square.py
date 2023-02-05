ab6c76e886d4cc22f1424c05c8dd828645871f8d
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-05 11:13:02 +0000

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
 
 class Square:
-    """This is a class representing a Square.
-
-    It contains methods and attributes to manage the size and position
-    of the square.
+    """Class to represent a Square.
+    
+    Attributes:
+    size (int): The size of the square.
+    position (tuple): A tuple representing the position of the square.
     """
-    def __init__(self, size=0):
-        """Initialize a Square with a given size.
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a Square with a given size and position.
         
         Args:
         size (int): The size of the square. Default is 0.
-
+        position (tuple): A tuple representing the position of the square. Default is (0, 0).
+        
         """
         self.size = size
+        self.position = position
+        
+    def area(self):
+        """Return the area of the square."""
+        return self.size ** 2
+
+    def my_print(self):
+        """Print the square to the standard output."""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
