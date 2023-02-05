f104622c14ee7a50321bd4ff785ab60f29fdf7c7
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-05 11:05:03 +0000

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
 class Square:
+    """This is a class representing a Square.
+
+    It contains methods and attributes to manage the size and position
+    of the square.
+    """
     pass
