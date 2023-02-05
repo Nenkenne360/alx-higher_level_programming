181be7217c3769db114471a99d002fc6529db344
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-05 11:10:47 +0000

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,7 +6,11 @@ class Square:
     It contains methods and attributes to manage the size and position
     of the square.
     """
-    pass
-    """
-    The class does nothing
-    """
+    def __init__(self, size=0):
+        """Initialize a Square with a given size.
+        
+        Args:
+        size (int): The size of the square. Default is 0.
+
+        """
+        self.size = size
