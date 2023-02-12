507ce773cd9a85ecf3c0b1cf520f0a3212252d92
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-12 14:38:21 +0000

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-'''1-square.py: Python script that defines square,
-   private instantiation attribute of size'''
+"""1-square.py: Python script that defines square,
+   private instantiation attribute of size"""
 
 
 class Square:
-        '''Creates  Square type'''
-
-        def __init__(self, size):
-            '''Initializes Square with size with,
-            no type/value verification'''
-
-            self.__size = size
-            
+    
+    """Creates  Square type"""
+    def __init__(self, size):
+        """ Initializes Square with size with,
+        no type/value verification"""
+        self.__size = size
