50a3b14745c55a020de8ae3fb82e6472e55d911d
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-20 16:27:50 +0000

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- no name order
-SELECT score, name, COUNT(*) AS number 
+SELECT score, name, COUNT(*) AS number
 FROM second_table
 WHERE name <> ''
-GROUP BY score, name 
+GROUP BY score, name
 ORDER BY number DESC;
