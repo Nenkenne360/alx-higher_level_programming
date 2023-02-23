8b768d48c7bc62b2213318b24ab7fe7654fc9565
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-23 08:52:43 +0000

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,2 @@
 -- cities by states
-SELECT c.id, c.name, s.name FROM cities c, states s
-WHERE c.state_id = s.id
-GROUP BY c.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
