87be99432de8e91cec9c73ff1f0ded15e61269ba
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-03-08 18:36:30 +0000

    Update 8-base_geometry.py

--- 0x0A-python-inheritance/8-base_geometry.py
+++ 0x0A-python-inheritance/8-base_geometry.py
@@ -11,8 +11,14 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
+    """Class representing a rectangle"""
+
     def __init__(self, width, height):
-        if super().integer_validator("width", width):
-            self.__width = width
-        if super().integer_validator("height", height):
-            self.__height = height
+        """Initialize a new Rectangle instance"""
+
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
